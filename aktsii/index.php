4b3040b6697182a1eb9f344b4b49f811880726ee
Tatyana <audtg@yandex.ru>
2017-01-29 16:55:39 +0300

    commit

--- aktsii/index.php
+++ aktsii/index.php
@@ -1,8 +1,6 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
-$APPLICATION->SetTitle("Акции");
+$APPLICATION->SetTitle("Акцииfetrdhgv");
 ?>
-
-Text here....
 
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
--- login/index.php
+++ login/index.php
@@ -1,13 +1,13 @@
 <?
 define("NEED_AUTH", true);
-require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
 
-if (isset($_REQUEST["backurl"]) && strlen($_REQUEST["backurl"])>0) 
-	LocalRedirect($backurl);
+if (isset($_REQUEST["backurl"]) && strlen($_REQUEST["backurl"]) > 0)
+    LocalRedirect($backurl);
 
 $APPLICATION->SetTitle("Вход на сайт");
 ?>
-<p>Вы зарегистрированы и успешно авторизовались.</p>
+    <p>Вы зарегистрированы и успешно авторизовались.</p>ssh
 
-<p><a href="<?=SITE_DIR?>">Вернуться на главную страницу</a></p>
-<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+    <p><a href="<?= SITE_DIR ?>">Вернуться на главную страницу</a></p>
+<? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
